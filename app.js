48e4cbc3a9a5e420560e5a9c2f49836e19e81872
imaishwaryaprabhu <imaishwaryaprabhu@gmail.com>
2020-11-02 23:01:03 +0530

    url fix

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ mongoose.connect('mongodb://localhost/picsagram', { useNewUrlParser: true, useUn
 app.use(express.json({ limit: '50mb', extended: false }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
+let frontendDir = __dirname + '/frontend';
+app.use("/images/posts", express.static(__dirname + "/backend/images/posts"));
+app.use("/", express.static(frontendDir));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -26,7 +30,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/images/posts", express.static(__dirname + "/backend/images/posts"));
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", userRoutes);
 
